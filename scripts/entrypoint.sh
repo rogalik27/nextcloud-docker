f64d97ada79346437e60266e68d12ac3d59004e9
Micha Ivanchenko <mykhailo.ivanchenko@netways.de>
2025-05-22 18:00:42 +0200

    Added more debug logging and cleaned out output

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -10,7 +10,6 @@ SSL_DIR="/etc/apache2/ssl"
 CRT_FILE=""
 KEY_FILE=""
 
-echo "Installing Nextcloud..."
 CRT_FILE=$(find "$SSL_DIR" -maxdepth 1 -type f -name '*.crt' | head -n1)
 KEY_FILE=$(find "$SSL_DIR" -maxdepth 1 -type f -name '*.key' | head -n1)
 
@@ -36,6 +35,9 @@ export SSL_KEY_PATH="$KEY_FILE"
 
 echo "[SSL Skript] Exportiert ENV SSL_CERT_PATH=$SSL_CERT_PATH"
 echo "[SSL Skript] Exportiert ENV SSL_KEY_PATH=$SSL_KEY_PATH"
+
+if [ -f /var/www/html/config/config.php ]; then
+echo "[Nextcloud Install] Nextcloud nicht gefunden. Installiere Nextcloud..."
 php /var/www/server/occ maintenance:install \
     --database="${db_driver}" \
     --database-name="${db_name}" \
@@ -45,18 +47,43 @@ php /var/www/server/occ maintenance:install \
     --admin-pass="${nc_password}" \
     --data-dir="/home/data" \
     --database-host="db"
+    if grep -q "'installed' => true" /var/www/html/config/config.php; then
+        echo "[Nextcloud Install] Nextcloud ist erfolgreich installiert worden"
+    else
+        echo "[Nextcloud Install] NExtcloud konnte nicht installiert werden."
+    fi
+else
+  echo "[Nextcloud Install] Nextcloud bereits installiert."
+fi
+
+
+touch /var/www/server/config/myconfig.config.php
+./config.sh > /var/www/server/config/myconfig.config.php
 
 chown -R www-data:www-data /home/data
-touch /var/www/server/config/myconfig.config.php
 touch /etc/apache2/sites-enabled/000-default.conf
-./config.sh > /var/www/server/config/myconfig.config.php
 ./apacheconfig.sh > /etc/apache2/sites-enabled/000-default.conf
+for file in \
+  /var/www/server/config/config.php \
+  /var/www/server/config/myconfig.config.php
+do
+if [ -f "$file" ]; then
+  if [ -s "$file" ]; then
+    echo "[Konfigurator] $file Ist vorhanden"
+  else
+    echo "[Konfigurator] $file Ist vorhanden, aber ist leer"
+  fi
+else
+  echo "[Konfigurator] $file Ist NICHT vorhanden"
+fi
+done
 
 chown -R www-data:www-data /var/www/server/config
-
-php /var/www/server/occ maintenance:repair --include-expensive
-
+echo "[Nextcloud Install] Nextcloud maintenance:repair Job wird durchgeführt"
+php /var/www/server/occ maintenance:repair --include-expensive > /dev/null
+echo "[Nextcloud Install] Nextcloud maintenance:repair Job ist zu Ende"
+echo "[Nextcloud Install]"
 sudo -u www-data /var/www/server/occ app:disable files_reminders
 
 echo "Ended script"
-exec apache2ctl -D FOREGROUND
+exec apache2ctl -D FOREGROUND 
